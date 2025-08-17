958bd792ee8ea2a3c9dcde08e2330d8428aacff0
legiorex <batyushev@gmail.com>
2025-08-18 07:47:47 +1000

    Remove unused HelloHandler implementation from internal package

--- cmd/main.go
+++ cmd/main.go
@@ -5,14 +5,12 @@ import (
 	"net/http"
 
 	"go-api/internal/auth"
-	"go-api/internal/hello"
 )
 
 func main() {
 	fmt.Println("Starting server on port 8081")
 
 	router := http.NewServeMux()
-	hello.NewHelloHandler(router)
 	auth.NewAuthHandler(router)
 	server := http.Server{
 		Addr:    ":8081",
